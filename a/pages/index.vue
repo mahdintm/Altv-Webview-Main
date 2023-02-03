7c189d594cbf31da72400e4383ac4c97228563da
Mahdi Nemati <nematimahdi88@gmail.com>
2023-02-03 22:56:12 +0330

    chat system imported to index view

--- a/pages/index.vue
+++ a/pages/index.vue
@@ -1,13 +1,28 @@
 <template>
-  <div>
-    <ChatSystem />
-  </div>
+  <b-container style="background-color: #111111; height: 100vh" fluid>
+    <b-row align-h="between" class="text-center h-50">
+      <b-col
+        cols="7"
+        sm="6"
+        md="6"
+        lg="5"
+        xl="4"
+        class="p-2"
+        style="height: 73%"
+        ><ChatSystem
+      /></b-col>
+      <b-col cols="3" sm="3" md="3" lg="2" xl="2" class="hud h-50"
+        >HUD <br />
+        HUD
+      </b-col>
+    </b-row>
+  </b-container>
 </template>
 
 <script>
 import ChatSystem from '~/components/chat/baseChat.vue'
 export default {
-  comments: [ChatSystem],
+  components: { ChatSystem },
   name: 'IndexPage',
   data() {
     return {
@@ -18,3 +33,8 @@ export default {
   },
 }
 </script>
+<style>
+.hud {
+  /* background-color: aqua; */
+}
+</style>
